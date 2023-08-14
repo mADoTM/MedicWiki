a3224a951b06b5091a95078d605b42d20135dd6b
Максим Долженко <maxim_2003@mail.ru>
2023-08-14 13:45:46 +0300

    feat: now UserRepository can find user by id

--- backend/src/main/java/ru/dolzhenkoms/backend/repository/UserRepository.java
+++ backend/src/main/java/ru/dolzhenkoms/backend/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByLogin(String name);
+
+    Optional<User> findById(long id);
 }
