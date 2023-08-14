7a5d1168e07829b046eaf487eff4dfb37793e589
Максим Долженко <maxim_2003@mail.ru>
2023-08-14 13:47:26 +0300

    feat: made ArticleServiceImpl for base methods

--- backend/src/main/java/ru/dolzhenkoms/backend/service/impl/ArticleServiceImpl.java
+++ backend/src/main/java/ru/dolzhenkoms/backend/service/impl/ArticleServiceImpl.java
@@ -2,52 +2,75 @@ package ru.dolzhenkoms.backend.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.dolzhenkoms.backend.dto.ArticleDetailsDto;
-import ru.dolzhenkoms.backend.dto.ArticleFeedDto;
-import ru.dolzhenkoms.backend.dto.MedicTermDto;
-import ru.dolzhenkoms.backend.dto.UserArticleDto;
+import ru.dolzhenkoms.backend.dto.*;
 import ru.dolzhenkoms.backend.entity.Article;
 import ru.dolzhenkoms.backend.entity.MedicTerm;
 import ru.dolzhenkoms.backend.entity.User;
 import ru.dolzhenkoms.backend.repository.ArticleRepository;
 import ru.dolzhenkoms.backend.service.ArticleService;
+import ru.dolzhenkoms.backend.service.UserService;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import javax.security.auth.login.AccountNotFoundException;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
-    private final ArticleRepository repository;
+    private final ArticleRepository articleRepository;
+
+    private final UserService userService;
 
     @Override
     public List<ArticleFeedDto> getArticleFeeds() {
-        return repository
+        return articleRepository
                 .findAll()
                 .stream()
                 .map(this::mapArticleToArticleFeed)
+                .limit(10)
                 .collect(Collectors.toList());
     }
 
     @Override
-    public Optional<ArticleDetailsDto> getArticleDetails(final String articleName) {
-        final var articleFromDb = repository.findArticleByName(articleName);
+    public ArticleDetailsDto getArticleDetailsById(Long id) {
+        final var articleFromDb = articleRepository.findArticleById(id);
 
         if (articleFromDb == null) {
-            return Optional.empty();
+            throw new EntityNotFoundException("The wrong article's name");
         }
 
-        final var dto = getArticleDetailsDto(articleFromDb);
+        return getArticleDetailsDto(articleFromDb);
+    }
 
-        return Optional.of(dto);
+    @Override
+    public CreateArticleResponseDto createArticle(CreateArticleRequestDto requestDto) {
+        if(articleRepository.findArticleByName(requestDto.getName()) != null) {
+            throw new EntityExistsException("There is an article with the same name");
+        }
+
+        final var userOptional = userService.findById(requestDto.getUserId());
+
+        if(userOptional.isEmpty()) {
+            throw new EntityNotFoundException("Wrong user details");
+        }
+
+        var persistArticle = mapCreateArticleRequestDtoToArticle(requestDto);
+        persistArticle.setAuthor(userOptional.get());
+        persistArticle.setViewsCount(0);
+
+        var savedArticle = articleRepository.save(persistArticle);
+
+        return new CreateArticleResponseDto(savedArticle.getId(), savedArticle.getName());
     }
 
     private ArticleDetailsDto getArticleDetailsDto(Article articleFromDb) {
         return ArticleDetailsDto.builder()
                 .author(
-                        mapUserToArticleDto(
+                        mapUserToUserArticleDto(
                                 articleFromDb.getAuthor()
                         )
                 )
@@ -55,7 +78,7 @@ public class ArticleServiceImpl implements ArticleService {
                         articleFromDb
                                 .getContributors()
                                 .stream()
-                                .map(this::mapUserToArticleDto)
+                                .map(this::mapUserToUserArticleDto)
                                 .toList()
                 )
                 .medicTerm(
@@ -74,7 +97,7 @@ public class ArticleServiceImpl implements ArticleService {
                 .build();
     }
 
-    private UserArticleDto mapUserToArticleDto(final User user) {
+    private UserArticleDto mapUserToUserArticleDto(final User user) {
         return UserArticleDto.builder()
                 .login(user.getLogin())
                 .name(user.getName())
@@ -88,6 +111,23 @@ public class ArticleServiceImpl implements ArticleService {
                 .application(medicTerm.getApplication())
                 .history(medicTerm.getHistory())
                 .photo(medicTerm.getPhoto())
+                .build();
+    }
+
+    private MedicTerm mapDtoToMedicTerm(final MedicTermDto medicTerm) {
+        return MedicTerm.builder()
+                .name(medicTerm.getName())
+                .history(medicTerm.getHistory())
+                .application(medicTerm.getApplication())
+                .photo(medicTerm.getPhoto())
+                .build();
+    }
+
+    private Article mapCreateArticleRequestDtoToArticle(final CreateArticleRequestDto dto) {
+        return Article.builder()
+                .name(dto.getName())
+                .medicTerm(mapDtoToMedicTerm(dto.getMedicTerm()))
+                .createdAt(LocalDate.now())
                 .build();
     }
 }
